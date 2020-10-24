ad70c30c3f1afc0a396375519bd4c03cd96b34ed
myGat <71548947+myGat@users.noreply.github.com>
2020-10-24 17:07:15 +0200

    Add files via upload

---
+++ debugCommands/scripts/outputModifier.js
@@ -0,0 +1,23 @@
+const modifier = (text) => {
+  if(state.modules.queryAI) for(module of modules) if(module.name === state.modules.queryModule && module.getQuery) {
+    state.modules.forceOutput = ""
+    module.getQuery(text)
+    if(state.memory.authorsNote === "") delete state.memory.authorsNote
+    return {text: state.modules.forceOutput}
+  }
+
+  for(i of state.modules.order) if(modules[i].process) modules[i].process("output")
+  let modifiedText = text
+  for(i of state.modules.order) if(modules[i].output) modifiedText = modules[i].output(modifiedText)
+  state.modules.contextIsContinue = true
+  if(state.modules.addToOut){
+    modifiedText = state.modules.addToOut + modifiedText;
+    delete state.modules.addToOut
+  }
+  if(state.memory.authorsNote === "") delete state.memory.authorsNote
+  return { text: modifiedText }
+}
+
+// Don't modify this part
+modifier(text)
+  
