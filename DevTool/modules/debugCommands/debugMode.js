7988d07bca2e5872deaac1c6c9437fb7cc7c3581
myGat <71548947+myGat@users.noreply.github.com>
2020-10-24 16:39:40 +0200

    Update debugMode.js

--- DevTool/modules/debugCommands/debugMode.js
+++ DevTool/modules/debugCommands/debugMode.js
@@ -1,5 +1,2 @@
 debugMode = true //set to false to deactivate the debug commands
 debugVerbosity = 0 //send to 1 to have some messages send by debug commands 
-
-// /exe -pi commandHelper.commandList.hello = { execute: function(args) { state.message = "Hello world!" }}
-// /exe state.debug.instructions.splice(0,1)
